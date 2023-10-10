e8e8485e5c56108ae77ab7ef3aa1a864071e5bc3
olamide.olaitan <olamide.olaitan@etranzact.com>
2023-10-10 15:04:15 +0100

    pkm experience updated

--- src/components/HomePage/PKMExperience.js
+++ src/components/HomePage/PKMExperience.js
@@ -20,7 +20,7 @@ const swiperParams = {
       slidesPerView: 3,
     },
   },
-  pagination: true,
+  pagination: false,
   spaceBetween: 5,
 };
 
@@ -71,17 +71,17 @@ export default function PocketmoniXperience() {
       <swiper-container
         init="false"
         ref={swiperRef}
-        className="bg-white w-full h-[200px] md:h-3/6 z-0"
+        className="bg-white w-[200%] h-[200px] md:h-3/6 z-0"
         // change the swiper bullets color to the same of the arrows
-        style={{ '--swiper-pagination-color': '#3b82f6', marginBottom: "10%", zIndex: 0, height:  "400px"  }}
+        style={{ '--swiper-pagination-color': '#3b82f6', marginBottom: "10%", zIndex: 0, height:  "400px", overflow: "hidden"  }}
       >
         {UserData.pkmexperience.map((item, index) => (
           <swiper-slide key={index}  className="w-[16rem] h-full swiper-1" controller-control=".swiper-2">
-            <Image src={item.img} alt="image1" className="sliderImages" style={{height: "100%", width: "100%"}}/>
+            <Image src={item.img} alt="image1" className="sliderImages" style={{height: "90%", width: "100%"}}/>
              
-          <div key={item.id} className="z-50 -mt-[85%] ml-6 w" style={{zIndex: 50}}>
-            <h2 className="md:text-[32px] text-[20px] font-switzer"> {item.title} </h2>
-            <p className="text-justify md:text-center md:text-base text-[12px] md:leading-7 mt-0 md:mt-2 font-switzer">
+          <div key={item.id} className="z-50 -mt-[70%] ml-6 w-[300px]" style={{zIndex: 50}}>
+            <h2 className="md:text-[42px] text-[20px] font-switzer font-extrabold text-white "> {item.title} </h2>
+            <p className="text-left md:text-left md:text-[12px] text-[12px] md:leading-7 mt-0 md:mt-2 font-switzer text-white">
               {item.desc}
             </p>
           </div>
@@ -90,20 +90,20 @@ export default function PocketmoniXperience() {
         
       </swiper-container>
       <div
-        className="flex justify-between w-3/6 mx-auto -mt-7"
+        className="flex justify-between w-1/6 mx-auto -mt-7"
         style={{ position: 'relative', zIndex: 6 }}
       >
         <button
           onClick={() => {           
             swiperRef.current.swiper.slidePrev();
           }}
-          className="fill-slate-400 text-grey font-extrabold text-5xl md:mr-16"
+          className="fill-slate-400 text-navy-200 font-extrabold text-5xl w-10 h-10 p-2 hover:rounded-full hover:border"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            stroke-width="3.5"
             stroke="currentColor"
             class="w-5 h-5"
           >
@@ -114,18 +114,29 @@ export default function PocketmoniXperience() {
             />
           </svg>
         </button>
+        <div className=' w-16 flex justify-between align-middle pt-3'>
+          <div className='h-3 bg-white w-3 rounded-full border-2 border-navy-400'>
+
+          </div>
+          <div className='h-3 bg-navy-200 w-3 rounded-full'>
+
+          </div>
+          <div className='h-3 bg-navy-200 w-3 rounded-full'>
+
+          </div>
+        </div>
         <button
           onClick={() => {
             swiperRef.current.swiper.slideNext();
 
           }}
-          className="fill-slate-400 text-grey font-extrabold"
+          className="fill-slate-400 text-navy-200 font-extrabold w-10 h-10 p-2 hover:rounded-full hover:border"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            stroke-width="3.5"
             stroke="currentColor"
             class="w-5 h-5"
           >
